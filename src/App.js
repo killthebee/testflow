5b64534b3df6cedbcdcd373dea89bd195bed211a
Danny <superhotfire447@yandex.ru>
2021-03-10 10:22:27 +0300

    dockerfiles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         HEllo THERE1345
         </p>
         <a
           className="App-link"
@@ -23,3 +23,5 @@ function App() {
 }
 
 export default App;
+
+// docker run -p 8000:3000 -v /app/node_modules -v C:\Users\Danny\PycharmProjects\prod\frontend:/app -e CHOKIDAR_USEPOLLING=true volumetest1
